852ff624edbb63ed3b343c27cb7bc2f622af9f46
Aayush Karki <147123757+akarki2005@users.noreply.github.com>
2025-02-03 10:22:58 -0500

    minor tweaks

--- src/components/NavBar/NavBarOptions.tsx
+++ src/components/NavBar/NavBarOptions.tsx
@@ -6,10 +6,10 @@ function NavBarOptions() {
             <Link to="/resume" className="flex items-center bg-slate-900 hover:bg-slate-950 hover:scale-105 transition delay-50 duration-200 rounded-l-md p-3 text-white">Resume</Link>
             <Link to="/projects" className="flex items-center bg-slate-900 hover:bg-slate-950 hover:scale-105 transition delay-50 duration-200 p-3 text-white">Projects</Link>
             <Link to="https://github.com/akarki2005" className="flex items-center bg-slate-900 hover:bg-slate-950 hover:scale-105 transition delay-50 duration-200 p-3 text-white">
-            <img className="w-5" src="/src/assets/images/github.256x249.png"></img>
+            <img className="w-6" src="/src/assets/images/github.256x249.png"></img>
             </Link>
             <Link to="https://www.linkedin.com/in/aayushkarki05/" className="flex items-center bg-slate-900 hover:bg-slate-950 hover:scale-105 transition delay-50 duration-200 rounded-r-md p-3 text-white">
-            <img className="w-5" src="/src/assets/images/linkedin.256x256.png"></img>
+            <img className="w-6" src="/src/assets/images/linkedin.256x256.png"></img>
             </Link>
         </div>
     )
--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -8,7 +8,7 @@ function ResumePage() {
         <div className="flex flex-row w-full justify-center">
             <ImageCard imgurl="/src/assets/images/resume.png" linkurl={"/src/assets/files/AayushKarki_Resume.pdf"}></ImageCard>
         </div>
-        <p className="text-center text-slate-800 m-4 text-xl">Current as of February 3rd, 2025.</p>
+        <p className="text-center text-slate-800 m-4 text-xl">*Current as of February 3rd, 2025.</p>
         </>
     )
 }
