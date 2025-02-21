aa98b1402a43f13ab554775e3a158130280e8acf
Aayush Karki <147123757+akarki2005@users.noreply.github.com>
2025-02-20 23:53:24 -0700

    more responsive design fixes

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -5,8 +5,10 @@ function ResumePage() {
         <>
         <h1 className="text-center text-slate-800 m-4 text-3xl font-bold">Resume</h1>
         <p className="text-center text-slate-800 m-4 text-xl">Click on the card below to download the latest version of my resume.</p>
-        <div className="flex flex-row w-4/5 justify-center justify-self-center">
-            <ImageCard imgurl="./assets/images/resume.png" linkurl={"/src/assets/files/AayushKarki_Resume.pdf"}></ImageCard>
+        <div className="flex justify-center">
+            <div className="md:w-1/2 w-4/5">
+            <ImageCard imgurl="./assets/images/resume.png" linkurl={"./assets/files/AayushKarki_Resume.pdf"}></ImageCard>
+            </div>
         </div>
         <p className="text-center text-slate-800 m-4 text-xl">*Current as of February 3rd, 2025.</p>
         </>
