49118f443f415b954fa792488c6ba10728ca2c10
Aayush Karki <147123757+akarki2005@users.noreply.github.com>
2025-02-21 12:11:31 -0700

    arraycard fix #2

--- src/components/ArrayCard/ArrayCardContent.tsx
+++ src/components/ArrayCard/ArrayCardContent.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 function ArrayCardContent(props: Props) {
     return (
-        <div className="flex flex-row justify-center bg-slate-800 rounded-b-xl p-2 h-2/3 grid md:grid-cols-5 grid-cols-3 place-items-center">
+        <div className="flex flex-wrap flex-row justify-center bg-slate-800 rounded-b-xl p-2 h-2/3">
             {props.content.map(card =>
                 <ILCard image_path={card[0]} link={card[1]} link_name={card[2]} ></ILCard>
             )}
